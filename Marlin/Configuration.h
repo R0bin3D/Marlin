cb326ee67908ee461fdbbe1efbb6213cdb8b355a
Robin3D <rseidel89@googlemail.com>
2020-09-09 21:37:04 +0200

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -71,7 +71,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(R0BIN3D, SKR 1.4 Turbo)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(R0BIN3D, SapphirePro-SKR 1.4 Turbo)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
